908741a90ab7289350485ef7cf5194af212e0e0a
Ната <nattaa2904@gmail.com>
2023-04-27 21:12:04 +0300

    немного универсальности
    ругается на не подходящие tiff
    
    ломает все неподходящие картинки, сделать алгоритм для перехода на новую полосу не вышло

--- rip_tiff/rip_tiff/Source.cpp
+++ rip_tiff/rip_tiff/Source.cpp
@@ -18,7 +18,7 @@ public:
         f = fopen(A, B);
         if (!f) {
             cout << "файл " << A << " не существует или не удалось создать\n";
-            throw 1;
+            throw exception("файл с таким именем не существует или не удалось создать\n");
         }
     };
 
@@ -35,123 +35,168 @@ public:
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    FileWithDes f1("0041_0102_01567_1_01497_03_S_fr.tiff", "rb");//создание потока для чтения tiff
-    //FileWithDes f1("0041_0102_01567_1_01497_03_S.tiff", "rb");//создание потока для чтения tiff
-    TiffFile initialFiile(f1.getF());//данные о tiff
+    try {
+        //FileWithDes f1("0041_0102_01567_1_01497_03_S_fr.tiff", "rb");//создание потока для чтения tiff
+        //FileWithDes f1("0041_0102_01567_1_01497_03_S.tiff", "rb");//создание потока для чтения tiff
+        FileWithDes f1("ket.tiff", "rb");//создание потока для чтения tiff
+        TiffFile initialFile(f1.getF());//данные о tiff
+        
 
-    int Add = 1;
-    cout << "пожалуйста, введите количество пикселей, которые будут объединены в 1 (сторона квадратной области)\n";
-    cin >> Add;
+        int Add = 1;
+        cout << "пожалуйста, введите количество пикселей, которые будут объединены в 1 (сторона квадратной области)\n";
+        cin >> Add;
 
-    if ((Add >= initialFiile.WIDTH) || (Add >= initialFiile.HIGHT))//проверка add
-    {
-        cout << "количество объединяемых пикселей слишком большое (изображение сожмется в картинку 1*1)";
-        return 0;
-    }
+        if ((Add >= initialFile.WIDTH) || (Add >= initialFile.HIGHT))//проверка add
+        {
+            throw exception("количество объединяемых пикселей слишком большое (изображение сожмется в картинку 1*1)");
+        }
 
-    FileWithDes f2("final.bmp", "wb");
-    //шапка .bmp
-    BITMAPFILEHEADER bfh;
-    BITMAPINFOHEADER bih;
-    uint8_t padding;
+        FileWithDes f2("final.bmp", "wb");
+        //шапка .bmp
+        BITMAPFILEHEADER bfh;
+        BITMAPINFOHEADER bih;
+        uint8_t padding;
 
-    int WOst = initialFiile.WIDTH % Add;
-    bih.biWidth = initialFiile.WIDTH / Add;
-    if (WOst) ++bih.biWidth;
+        int WOst = initialFile.WIDTH % Add;
+        bih.biWidth = initialFile.WIDTH / Add;
+        if (WOst) ++bih.biWidth;
 
-    int HOst = initialFiile.HIGHT % Add;
-    bih.biHeight = initialFiile.HIGHT / Add;
-    if (HOst) ++bih.biHeight;
+        int HOst = initialFile.HIGHT % Add;
+        bih.biHeight = initialFile.HIGHT / Add;
+        if (HOst) ++bih.biHeight;
 
-    bfh.bfType = 19778;
-    bfh.bfSize = sizeof(bfh) + sizeof(bih) + 3 * bih.biWidth * bih.biWidth;
-    bfh.bfReserved1 = 0;
-    bfh.bfReserved2 = 0;
-    bfh.bfOffBits = sizeof(bfh) + sizeof(bih);
+        bfh.bfType = 19778;
+        bfh.bfSize = sizeof(bfh) + sizeof(bih) + 3 * bih.biWidth * bih.biWidth;
+        bfh.bfReserved1 = 0;
+        bfh.bfReserved2 = 0;
+        bfh.bfOffBits = sizeof(bfh) + sizeof(bih);
 
-    bih.biSize = sizeof(bih);
-    bih.biPlanes = 1;
-    bih.biBitCount = 24;
-    bih.biCompression = 0;
-    bih.biSizeImage = bih.biWidth * bih.biWidth * 3;
-    bih.biXPelsPerMeter = 2835;
-    bih.biYPelsPerMeter = 2835;
-    bih.biClrUsed = 0;
-    bih.biClrImportant = 0;
+        bih.biSize = sizeof(bih);
+        bih.biPlanes = 1;
+        bih.biBitCount = 24;
+        bih.biCompression = 0;
+        bih.biSizeImage = bih.biWidth * bih.biWidth * 3;
+        bih.biXPelsPerMeter = 2835;
+        bih.biYPelsPerMeter = 2835;
+        bih.biClrUsed = 0;
+        bih.biClrImportant = 0;
 
-    padding = (4 - (bih.biWidth * 3) % 4) % 4;
+        padding = (4 - (bih.biWidth * 3) % 4) % 4;
 
-    fwrite(&bfh, sizeof(bfh), 1, f2.getF());
-    fwrite(&bih, sizeof(bih), 1, f2.getF());
-    
-    cout << "было: " << initialFiile.HIGHT << "*" << initialFiile.WIDTH << endl;
-    cout << "стало: " << bih.biHeight << "*" << bih.biWidth << endl;
-    cout << "выравнивание: " << int(padding) << endl;
+        fwrite(&bfh, sizeof(bfh), 1, f2.getF());
+        fwrite(&bih, sizeof(bih), 1, f2.getF());
 
-    vector<WORD> line1(initialFiile.WIDTH * 3 * Add);
-    vector<BYTE> line2(bih.biWidth * 3 + padding);
-    memset(line2.data() + bih.biWidth * 3, 0, padding * sizeof(BYTE));//паддинг line2 черный
-    vector <vector<BYTE>> matrBMP(bih.biHeight);
+        cout << "было: " << initialFile.HIGHT << "*" << initialFile.WIDTH << endl;
+        cout << "стало: " << bih.biHeight << "*" << bih.biWidth << endl;
+        cout << "выравнивание: " << int(padding) << endl;
 
-    double AverageB;
-    double AverageG;
-    double AverageR;
+        vector<WORD> line1(initialFile.WIDTH * 3 * Add);
+        vector<BYTE> line2(bih.biWidth * 3 + padding);
+        memset(line2.data() + bih.biWidth * 3, 0, padding * sizeof(BYTE));//паддинг line2 черный
+        vector <vector<BYTE>> matrBMP(bih.biHeight);
 
-    auto CreatePixel = [&](int allowH, int allowW, int position) {//создаёт каждый отдельный пиксель
-        int count = allowH * allowW;
-        AverageB = 0;
-        AverageG = 0;
-        AverageR = 0;
-        for (int j = 0; j < allowH; ++j) {
-            for (int k = 0; k < allowW; ++k) {
-                //суммирование по пикселям
-                AverageB += line1[j * (initialFiile.WIDTH * 3) + (position * Add + k) * 3 + 2] / 4;//B
-                AverageG += line1[j * (initialFiile.WIDTH * 3) + (position * Add + k) * 3 + 1] / 4;//G
-                AverageR += line1[j * (initialFiile.WIDTH * 3) + (position * Add + k) * 3] / 4;//R
+        double AverageB;
+        double AverageG;
+        double AverageR;
+
+        auto CreatePixel = [&](int allowH, int allowW, int position) {//создаёт каждый отдельный пиксель
+            int count = allowH * allowW;
+            AverageB = 0;
+            AverageG = 0;
+            AverageR = 0;
+            for (int j = 0; j < allowH; ++j) {
+                for (int k = 0; k < allowW; ++k) {
+                    //суммирование по пикселям
+                    AverageB += line1[j * (initialFile.WIDTH * 3) + (position * Add + k) * 3 + 2] / 4;//B
+                    AverageG += line1[j * (initialFile.WIDTH * 3) + (position * Add + k) * 3 + 1] / 4;//G
+                    AverageR += line1[j * (initialFile.WIDTH * 3) + (position * Add + k) * 3] / 4;//R
+                }
             }
-        }
-        AverageB /= count;
-        AverageG /= count;
-        AverageR /= count;
-        line2.data()[position * 3] = byte(AverageB + 0.5);
-        line2.data()[position * 3 + 1] = byte(AverageG + 0.5);
-        line2.data()[position * 3 + 2] = byte(AverageR + 0.5);
-    };
+            AverageB /= count;
+            AverageG /= count;
+            AverageR /= count;
+            line2.data()[position * 3] = byte(AverageB + 0.5);
+            line2.data()[position * 3 + 1] = byte(AverageG + 0.5);
+            line2.data()[position * 3 + 2] = byte(AverageR + 0.5);
+        };
 
-    auto CreateLine2 = [&](int h1, int w2) {//создаёт всю строку кроме остатка
-        for (int i = 0; i < w2; ++i) {
-            CreatePixel(h1, Add, i);
-        }
-    };
+        auto CreateLine2 = [&](int h1, int w2) {//создаёт всю строку кроме остатка
+            for (int i = 0; i < w2; ++i) {
+                CreatePixel(h1, Add, i);
+            }
+        };
 
-    auto CreateAllLine2 = [&](int h1) {
-        if (WOst)
+        auto CreateAllLine2 = [&](int h1) {
+            if (WOst)
+            {
+                CreateLine2(h1, bih.biWidth - 1);//создаёт всю строку кроме последнего пикселя
+                CreatePixel(h1, WOst, bih.biWidth - 1);//последний пиксель в строке
+            }
+            else
+            {
+                CreateLine2(h1, bih.biWidth);//создаёт всю строку
+            }
+        };
+
+        fseek(f1.getF(), initialFile.startsStrips[0], SEEK_SET);
+
+        
+        
+        //for (int i = 0; i < initialFile.HIGHT; i+=done) {
+            /*for (int j = 0; j < initialFile.countOfStripes; ++j) {
+                fseek(f1.getF(), initialFile.startsStrips[j], SEEK_SET);
+                if (Add - done < remained) {
+                    fread(line1.data(), (initialFile.WIDTH * 3 * 2) * (Add-done), 1, f1.getF());
+                    CreateAllLine2(Add);
+                    matrBMP[k] = line2;
+                    ++k;
+                    remained -= Add;
+                }
+                else ()
+            }*/
+        //}
+
+        /*int remained = 0;
+        int kRead = 0;
+        int done = 0;
+        int kWrite = 0;
+        for (int i = 0; i < initialFile.HIGHT; ++i) {
+            fread(line1.data(), (initialFile.WIDTH * 3 * 2), 1, f1.getF());
+            ++remained;
+            ++done;
+            if ((remained == initialFile.stringsInStripe) && (kRead < initialFile.countOfStripes)) {
+                remained = 0;
+                ++kRead;
+                fseek(f1.getF(), initialFile.startsStrips[kRead], SEEK_SET);
+            }
+            if (done == Add) {
+                done = 0;
+                CreateAllLine2(Add);
+                matrBMP[kWrite] = line2;
+                ++kWrite;
+            }
+        }*/
+        
+
+        for (int i = 0; i < initialFile.HIGHT / Add; ++i) {
+            fread(line1.data(), (initialFile.WIDTH * 3 * 2) * Add, 1, f1.getF());
+            CreateAllLine2(Add);
+            matrBMP[i] = line2;
+        }
+
+        if (HOst)
         {
-            CreateLine2(h1, bih.biWidth - 1);//создаёт всю строку кроме последнего пикселя
-            CreatePixel(h1, WOst, bih.biWidth - 1);//последний пиксель в строке
+            CreateAllLine2(HOst);
+            matrBMP[bih.biHeight - 1] = line2;
         }
-        else
-        {
-            CreateLine2(h1, bih.biWidth);//создаёт всю строку
+
+        for (int i = bih.biHeight - 1; i >= 0; --i) {
+            fwrite(matrBMP[i].data(), bih.biWidth * 3 + padding, 1, f2.getF());
         }
+    }
+    catch (exception& ex) {
+        cout << "что-то пошло не так...\n\n";
+        cout<<ex.what();
     };
-
-    fseek(f1.getF(), initialFiile.start, SEEK_SET);//где хранится ссылка на фактическое начало изображения?
-
-    for (int i = 0; i < initialFiile.HIGHT / Add; ++i) {
-        fread(line1.data(), (initialFiile.WIDTH * 3 * 2)* Add, 1, f1.getF());
-        CreateAllLine2(Add);
-        matrBMP[i] = line2;
-    }
-
-    if (HOst)
-    {
-        CreateAllLine2(HOst);
-        matrBMP[bih.biHeight-1] = line2;
-    }
-
-    for (int i = bih.biHeight - 1; i >= 0; --i) {
-        fwrite(matrBMP[i].data(), bih.biWidth * 3 + padding, 1, f2.getF());
-    }
     return 0;
 }
--- rip_tiff/rip_tiff/TIFF.h
+++ rip_tiff/rip_tiff/TIFF.h
@@ -6,23 +6,24 @@ using namespace std;
 
 class TiffFile {
 public:
-    size_t HIGHT;
-    size_t WIDTH;
-    int bitOnPix;
-    uint32_t start;
+    size_t HIGHT = 0;//высота
+    size_t WIDTH = 0;//ширина
+    vector<uint32_t> startsStrips;
+    int countOfStripes = 0;
+    int stringsInStripe = 0;
 
     TiffFile(FILE* f) {//читает header
-        #pragma pac(push, 1);
+        #pragma pack(push, 1)
         struct HeadOfTiff {
             uint16_t IIorMM;
             uint16_t isItTiff;
             uint32_t offset;
         } head;
-        #pragma pac(pop);
+        #pragma pack(pop)
 
-        uint16_t count;
+        uint16_t count;//количество тегов
 
-        #pragma pac(push, 1);
+        #pragma pack(push, 1)
         struct IFD {
             uint16_t tag;
             uint16_t type;
@@ -34,17 +35,20 @@ public:
                 cout << "countOfType = " << countOfType << endl;
                 cout << "valueOffset = " << valueOffset << endl;
             }
-        }ifd;
-        #pragma pac(pop);
+        };
+        #pragma pack(pop)
 
-        fread(&head, sizeof(head), 1, f);
+        int bitOnPix = 0;//количество бит на канал
+        uint32_t startOfPtrToStripes = 0;
 
-        fseek(f, head.offset, SEEK_SET);
+        fread(&head, sizeof(head), 1, f);//чтение заголовка файла
+
+        fseek(f, head.offset, SEEK_SET);//переход и чтение вектора тегов
         fread(&count, 2, 1, f);
 
-        IFD* tags = new IFD[count];
+        vector<IFD> tags(count);//вектор тегов
 
-        fread(tags, sizeof(ifd), count, f);
+        fread(tags.data(), sizeof(IFD), count, f);//чтение тегов
 
         for (int i = 0; i < count; ++i)
         {
@@ -60,18 +64,44 @@ public:
                 fseek(f, tags[i].valueOffset, SEEK_SET);
                 fread(&bitOnPix, 2, 1, f);//2 соответствует типу
                 break;
+            case 259:
+                if (tags[i].valueOffset != 1)
+                    throw exception("файл сжат!");
+                break;
+            case 262:
+                if (tags[i].valueOffset != 1)
+                    throw exception("чёрный кодируется 255, т.е. все цвета инвертированы");
+                break;
             case 273:
-                fseek(f, tags[i].valueOffset, SEEK_SET);
-                fread(&start, 4, 1, f);//4 соответствует типу
+                startOfPtrToStripes = tags[i].valueOffset;//нам пока неизвестно количество полос
+                break;
+            case 277:
+                if (tags[i].valueOffset != 3)
+                    throw exception("в изображении не 3 канала и его не выйдет корректно преобразовать в пнг!");
+                break;
+            case 278:
+                stringsInStripe = tags[i].valueOffset;
+                cout << "строк в полосе: " << stringsInStripe << endl;
                 break;
             default:
                 break;
             }
         }
 
+        countOfStripes = HIGHT / stringsInStripe;
+        countOfStripes += (HIGHT % stringsInStripe) ? 1 : 0;
+        startsStrips.resize(countOfStripes);
+        fseek(f, startOfPtrToStripes, SEEK_SET);
+        fread(startsStrips.data(), 4, countOfStripes, f);//4 соответствует типу
+        cout << "количество полос: " << countOfStripes << endl;
+
+        if (!HIGHT || !WIDTH || !bitOnPix||!startOfPtrToStripes||!sizeof(startsStrips)||!stringsInStripe) {//если нет какого-то тега 
+            throw exception("нет всех необходимых тегов");
+        }
+        
         cout << "высота = " << HIGHT << endl;
         cout << "ширина = " << WIDTH << endl;
-        cout << "смещение к началу пикселей = " << start << endl;
+        //cout << "смещение к началу пикселей = " << start << endl;
         cout << "бит на канал = " << bitOnPix << endl;
 
         auto print = [&]() {
@@ -88,7 +118,5 @@ public:
         };
 
         //print();
-
-        delete[] tags;
     };
 };
